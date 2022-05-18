9c12dea000e062052ddf4b28ec66799dadfa5a44
JettScythe <jettscythe@hotmail.com>
2022-05-18 17:29:46 -0300

    refactor(commands.py): adds logging to /apy command + ran pre-commit hooks

--- src/commands.py
+++ src/commands.py
@@ -1,7 +1,14 @@
+import logging
+
 from telegram import Update
 from telegram.ext import CallbackContext
+
 from helpers import get_apy
+
 
 async def apy(update: Update, context: CallbackContext.DEFAULT_TYPE):
     apy = await get_apy()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="FlexUSD AvgAPY: {}%".format(apy))
+    logging.info("Sent message to {}".format(update.effective_chat.id))
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="FlexUSD AvgAPY: {}%".format(apy)
+    )
