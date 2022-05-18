70d9eea8e23797a5f3451301bd814c9b01eb8f68
JettScythe <jettscythe@hotmail.com>
2022-05-18 10:53:38 -0300

    PoC fin

--- src/flexbot.py
+++ src/flexbot.py
@@ -1,3 +1,31 @@
 import requests
-import json
-import time
+from dotenv import dotenv_values
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler
+
+config = dotenv_values(".env")
+
+
+logging.basicConfig(
+format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+level=logging.INFO
+)
+
+
+async def get_apy():
+    apy_req = requests.get(config["APIURL"]).json()
+    apy = (float(apy_req['data'][0]['avgApr']) * 100)
+    return apy
+
+
+async def apy(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    apy = await get_apy()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="FlexUSD AvgAPY: {}%".format(apy))
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(config['TGTOKEN']).build()
+    apy_handler = CommandHandler('apy', apy)
+    application.add_handler(apy_handler)
+    application.run_polling()
